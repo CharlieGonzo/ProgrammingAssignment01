653b162ff27f0360b936a78c19a1cb3d85340861
charliegonzo <gonzc3@farmingdale.edu>
2026-01-29 11:47:31 -0500

    TODO #2

--- src/main/java/org/example/HardwareTicket.java
+++ src/main/java/org/example/HardwareTicket.java
@@ -29,7 +29,10 @@ public class HardwareTicket extends Ticket {
     // -----------------------------------------
     @Override
     public int urgencyScore() {
-        // TODO #2
-        return -1;
+        int score = super.getPriority()*10 + getDaysOpen()*2;
+        score += (labCritical ? 20 : 0);
+        score += Math.min(affectedUsers, 30);
+        score += (deviceType.equalsIgnoreCase("Printer") ? 5 : 0);
+        return score;
     }
 }
